370018f3bd3c7fb6904aa575281674f0527059e8
hewydewyle <le.hieu@gmail.com>
2014-10-25 19:34:03 +0000

    warning box #27

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,10 +13,15 @@ class PlacesController < ApplicationController
     end
 
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
+		@place = current_user.places.create(place_params)
+		if @place.valid?
+		  redirect_to root_path
+		else
+			render :new, :status => :unprocessable_entity
+		end
 	end
 
+	
 	def show
 		@place = Place.find(params[:id])
 	end
@@ -32,12 +37,18 @@ class PlacesController < ApplicationController
 
 	def update
 		@place = Place.find(params[:id])
+
 		if @place.user != current_user
 			return render :text => 'Not Allowed', :status => :forbidden
 		end
 
 		@place.update_attributes(place_params)
-		redirect_to root_path
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :edit, :status => :unprocessable_entity
+		end
+
 	end
 
 	def destroy
